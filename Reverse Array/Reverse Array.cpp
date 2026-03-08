aa46615346a002cf4f02601a03b5af0f46d22feb
jm underwood <jm.underwood@hotmail.com>
2026-03-08 18:44:04 -0400

    Added in some comments.

--- Reverse Array/Reverse Array.cpp
+++ Reverse Array/Reverse Array.cpp
@@ -8,17 +8,21 @@
 int* ReverseArray(const int arr[], int size);
 void print(const int arr[], int size);
 
+// The purpose of this functon is to populate an array 
+// and then dynamcially allocate memory for a second array
+// that has the same value as first but in reverse oder
 int main()
 {
+    //Setting size for array
     const int SIZE = 5;
+
+    //FIlling the array
     const int array[SIZE] = { 1,2,3,4,5 };
 
-    std::cout << &SIZE << std::endl;
-    std::cout << "Code is running" << std::endl;
-
+    //Uses the ReverseArray function to dynamically
+    //allocate the memory for the reverse array
     int *rev_array = ReverseArray(array, SIZE);
 
-    std::cout << rev_array << std::endl;
     
     std::cout << "Original Array" << std::endl;
     print(array, SIZE);
@@ -26,15 +30,21 @@ int main()
     std::cout << std::endl << "Reversed Array" << std::endl;
     print(rev_array, SIZE);
 
+    //Deallocate memory for the reverse array
     delete[] rev_array;
 
-    std::cout << *rev_array << std::endl;
 }
 
+//Returns int pointer for the reverse array
 int* ReverseArray(const int arr[], int size)
 {
+    //Dynamically Allocates memory for the rariable revarray
+
     int* revarry = new int[size];
 
+    
+    //Populates values for revarry with reverse values of
+    //the initial array
     for (int j = 0; j < size; j++)
     {
         revarry[j] = arr[size-1 - j];
@@ -45,6 +55,7 @@ int* ReverseArray(const int arr[], int size)
 
 void print(const int arr[], int size)
 {
+    //Prints out the values of the array
     for (int k = 0; k < size; k++)
     {
         std::cout << std::setw(4) << arr[k] << std::endl;
